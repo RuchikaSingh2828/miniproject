bbf00749a3b4c30abed3b00f89e3adf27b91fce2
Ruchika Singh <ruchika.singh2820@gmail.com>
2021-05-13 12:07:27 +0530

    Form +  Modal [Create Project modal ]
    Addition of form + modal changes for Create Project Modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import Button from './components/Button';
+import Modal from './components/Modal';
 import React, {useState} from 'react';
 
 function App() {
   const [isModalOpen, updateIsModalOpen] = useState(false);
 
-  const openModal= () => {}
+  const openModal= () => {
+    updateIsModalOpen(true);
+  }
+
+  const handleClose = () => {
+    updateIsModalOpen(false);
+  }
+  const handleSubmit = (data) => {
+    updateIsModalOpen(false);
+  }
 
   return (
     <div className="App">
-        <Button text="Describe Project" handler={openModal} />
+        <Button text="Describe Project" handler={openModal} styleClass="centered button"/>
+        <Modal show={isModalOpen} onHide={handleClose}/>
     </div>
   );
 }
--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
- const button = ({ text, handler }) => {
-    return <Button className="button centered" onClick={ handler }>{ text }</Button> 
+ const button = ({ text, handler, styleClass, disabled=false}) => {
+    return <Button disabled={disabled} className={`${styleClass}`} onClick={ handler }>{ text }</Button> 
 }
 
 export default button;
